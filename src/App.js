10781dd8ff70af3614abcdfa7bcd7bcbb643178f
Tomer Ovadia <o.tomerr@gmail.com>
2019-02-08 19:59:48 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.scss';
+
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <div className="container">
+            <div className="name">
+              <span>T</span>
+              <span>o</span>
+              <span>d</span>
+              <span>a</span>
+              <span>i</span>
+              <span>l</span>
+              <span>y</span>
+            </div>
+            <div className="line" />
+            <div className="sub">
+              <p>Effective Daily Standups</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
