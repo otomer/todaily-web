c5dd564d053cafab2dba580e6273e15952ed58a0
Tomer Ovadia <o.tomerr@gmail.com>
2019-02-09 01:41:56 +0200

    Update minisite

--- src/App.js
+++ src/App.js
@@ -20,8 +20,49 @@ class App extends Component {
             <div className="line" />
             <div className="sub">
               <p>Effective Daily Standups</p>
+              <p className="description">
+                Enjoy the{' '}
+                <a href="https://chrome.google.com/webstore/detail/todaily/aidlimhbkbbifgjlihhhakplpgfoljmh">
+                  <img alt="" width="19" height="19" src="./chrome.png" />
+                </a>{' '}
+                Extension :)
+              </p>
             </div>
           </div>
+
+          <footer id="target">
+            <div className="content">
+              <div className="top">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://chrome.google.com/webstore/detail/todaily/aidlimhbkbbifgjlihhhakplpgfoljmh"
+                >
+                  <i className="fab fa-chrome" />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/otomer/"
+                >
+                  <i className="fab fa-github" />
+                </a>
+              </div>
+              <div className="bottom">
+                <label>
+                  made with <i className="fas fa-heart beat" /> by{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.tomerovadia.com"
+                  >
+                    Tomer Ovadia
+                  </a>{' '}
+                  @ 2019
+                </label>
+              </div>
+            </div>
+          </footer>
         </div>
       </div>
     );
